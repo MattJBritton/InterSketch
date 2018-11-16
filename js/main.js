d66d8bfdd4576aeb9ab3516090fac2ceffab7e5a
Matt Britton <matt@lawn-143-215-85-39.lawn.gatech.edu>
2018-11-16 13:18:26 -0500

    Return matched curves on sketch. Also event data is now loaded.

--- js/main.js
+++ js/main.js
@@ -37,14 +37,117 @@
     }))
   }
 
-  d3.json("static/entries.json")
-  .then(raw_data => {
+  //distance function for DTW
+  distFunc = function( a, b ) {
+    return Math.abs( a - b );
+  };
+
+  function roundForDTW(d) {
+    return Math.round(d*10)/10.;
+  }
+
+  function calculateDTW(svg, timeline, sketch, data){
+
+      mapDTW = {}
+      //turn sketch into X:Y dict
+      mapSketch = {}
+      sketch.forEach(d => mapSketch[roundForDTW(d[0])] = d[1]);
+
+      //iterate through each curve and find points in the curve with the same X
+      //DTW library needs exact pairs
+      Object.entries(data).forEach(outer => {
+
+        tempCoordinates = [];
+        tempSketchCoordinates = [];
+        key = outer[0];
+        value = outer[1];
+
+        value.curve.forEach(inner =>{
+
+          x = roundForDTW(inner.x);
+          if(x in mapSketch) {
+
+            tempCoordinates.push(inner.y);
+            tempSketchCoordinates.push(mapSketch[x]);
+          }
+        })
+
+        mapDTW[key] = {
+          'sketch': tempSketchCoordinates, 
+          'data' : tempCoordinates
+        };
+      })
+
+      //calculate DTW for each series
+      Object.entries(mapDTW).forEach(d=> {
+
+        key = d[0];
+        value = d[1];
+        var dist = new DynamicTimeWarping(
+                                        value['data'], 
+                                        value['sketch'], 
+                                        distFunc
+                                        ).getDistance();
+
+        //arbitrary treshold for now
+        data[key].match = (dist/value['data'].length <= 25);
+      })
+
+      svg
+        .datum({
+          series: data,
+          sketch: [],
+        })
+        .call(timeline);      
+  }
+  Promise.all([
+    d3.json("static/treatments.json"),
+    d3.json("static/entries.json")
+  ])
+  .then(([events, series]) => {
+
+    console.log(events);
+
+    events_grouped = d3.nest()
+      .key(d => d.date)
+      .sortValues((a,b) => parseFloat(a.time) - parseFloat(b.time))
+      .entries(
+        events
+        .filter(d =>
+          d.hasOwnProperty('created_at') 
+          && d.hasOwnProperty('eventType')
+          && strictIsoParse(d.created_at.replace(" ","T")).getTime()
+            >= startDate.getTime()
+          && strictIsoParse(d.created_at.replace(" ","T")).getTime()
+            <= endDate.getTime())
+        .filter(d =>
+          (d.hasOwnProperty("insulin") && d.insulin != null) //ignore duplicate GCM readings in the treatment file
+          || (d.hasOwnProperty("carbs") && d.carbs != null)
+        )
+        .map(d => ({
+            date : moment.parseZone(d.created_at.replace(" ","T"))
+              .startOf("day")._d,
+            time : +d.created_at.substring(11,13) 
+              + d.created_at.substring(14,16)/60,
+            eventType : d.eventType  
+          })
+        )
+      )    
+      .map(group => ({
+          date: group.key,
+          events: group.values.map(
+            d => ({x: d.time, eventType:d.eventType}))
+        })
+      )
+
+    eventMap = {};
+    events_grouped.forEach(d => eventMap[d.date] = d.events );
 
     return d3.nest()
       .key(d => d.date)
       .sortValues((a,b) => parseFloat(a.time) - parseFloat(b.time))
       .entries(
-        raw_data
+        series
         .filter(d =>
           d.hasOwnProperty('dateString') 
           && d.hasOwnProperty('sgv')
@@ -53,7 +156,6 @@
           && strictIsoParse(d.dateString.replace(" ","T")).getTime()
             <= endDate.getTime())
         .map(d => ({
-            //date : formatTime(strictIsoParse(d.dateString.replace(" ","T"))),
             date : moment.parseZone(d.dateString.replace(" ","T"))
               .startOf("day")._d,
             time : +d.dateString.substring(11,13) 
@@ -65,7 +167,8 @@
       .map(group => ({
           date: group.key,
           curve: interpolate(group.values.map(
-            d => ({x: d.time, y:d.value})))
+            d => ({x: d.time, y:d.value}))),
+          events: eventMap[group.key]
         })
       //only return days that have close to a full 24 hours of data
       ).filter( group => d3.min(group.curve, d=> d.x) <= 1
@@ -75,6 +178,8 @@
   .then(series => {
 
     console.log(series);
+
+    svg = d3.select("#timeline");
 
     // Create the main timeline component.
     const now = moment();
@@ -89,17 +194,18 @@
       .seriesKey(s => now.diff(moment(s.date), 'days'))
       .seriesData(s => s.curve)
       .on('sketchStart', (curve) => {
-        console.log('sketchStart:', curve);
+        //console.log('sketchStart:', curve);
       })
       .on('sketch', (curve) => {
-        console.log('sketch:', curve);
+        //console.log('sketch:', curve);
       })
-      .on('sketchEnd', (curve) => {
+      .on('sketchEnd', function(curve){
         console.log('sketchEnd', curve);
+        calculateDTW(svg, mainTimeline, curve, series);
       });
 
     // Example: render the main timeline component.
-    d3.select('#timeline')
+    svg
       .datum({
         series: series,
         sketch: [],
